9370429c10d230710335d1655472d88a4cabea32
vellamitesh <miteshdongra4@gmail.com>
2025-09-12 19:46:24 +0530

    Update second.js

--- second.js
+++ second.js
@@ -2,3 +2,15 @@
   document.getElementById("loader").style.display = "none";
   document.getElementById("content").style.display = "block";
 });
+
+window.addEventListener("load", () => {
+  // Page load hone ke 3s baad VHS video load hoga
+  setTimeout(() => {
+    const effect = document.getElementById("effect");
+    const source = document.createElement("source");
+    source.src = "vi/lowVhs.mp4";
+    source.type = "video/mp4";
+    effect.appendChild(source);
+    effect.load();
+  }, 3000);
+});
